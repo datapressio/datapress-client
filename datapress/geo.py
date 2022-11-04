312cc7f2adb1329d9733c9f50c699b29bf14fa89
Joe Rees <joedrees@gmail.com>
2022-11-04 16:53:22 +0000

    added a function to get lad codes in a UTLA

--- datapress/geo.py
+++ datapress/geo.py
@@ -6,9 +6,24 @@ DIRNAME = os.path.dirname(os.path.abspath(__file__))
 """
 List the LSOAs in a given header region (as numpy array)
 """
+
+
 def get_lsoas_in(place):
     from pandas import read_csv
     filename = os.path.join(DIRNAME, 'static', 'lsoa_lookup.csv')
     frame = read_csv(filename)
     frame = frame[frame['UTLA19NM'] == place]
     return frame['LSOA11CD'].to_numpy()
+
+
+"""
+List the LADs in a given header region (as numpy array)
+"""
+
+
+def get_lads_in(place):
+    from pandas import read_csv
+    filename = os.path.join(DIRNAME, 'static', 'lsoa_lookup.csv')
+    frame = read_csv(filename)
+    frame = frame[frame['UTLA19NM'] == place]
+    return frame['LAD19CD'].to_numpy()
