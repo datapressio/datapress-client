08e0783a25ccb7d43319e965029301f708fcff72
Joe Rees <joedrees@gmail.com>
2022-11-16 15:37:24 +0000

    updated geo api, so it only returns unqiue values of a subset

--- datapress/geo.py
+++ datapress/geo.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 # datapress_client directory
 DIRNAME = os.path.dirname(os.path.abspath(__file__))
@@ -9,11 +10,10 @@ List the LSOAs in a given header region (as numpy array)
 
 
 def get_lsoas_in(place):
-    from pandas import read_csv
     filename = os.path.join(DIRNAME, 'static', 'lsoa_lookup.csv')
-    frame = read_csv(filename)
+    frame = pd.read_csv(filename)
     frame = frame[frame['UTLA19NM'] == place]
-    return frame['LSOA11CD'].to_numpy()
+    return frame['LSOA11CD'].unique()
 
 
 """
@@ -22,8 +22,14 @@ List the LADs in a given header region (as numpy array)
 
 
 def get_lads_in(place):
-    from pandas import read_csv
     filename = os.path.join(DIRNAME, 'static', 'lsoa_lookup.csv')
-    frame = read_csv(filename)
+    frame = pd.read_csv(filename)
     frame = frame[frame['UTLA19NM'] == place]
-    return frame['LAD19CD'].to_numpy()
+    return frame['LAD19CD'].unique().tolist()
+
+
+def get_geography_code(place):
+    filename = os.path.join(DIRNAME, 'static', 'lsoa_lookup.csv')
+    frame = pd.read_csv(filename)
+    frame = frame[frame['UTLA19NM'] == place]
+    return frame['UTLA19CD'].unique().tolist()
