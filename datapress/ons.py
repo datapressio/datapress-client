be8b48e584a46a4cc6799de28aa80c5b89e9d51a
Joe Rees <joedrees@gmail.com>
2022-11-16 15:39:02 +0000

    added more ons functions to use the filter functions rather than the quicker JSON API, v to 0.2.5...

--- datapress/ons.py
+++ datapress/ons.py
@@ -1,5 +1,17 @@
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+# Set the logging level by environment variable
+logging_level = os.environ.get('LOGLEVEL') or 'INFO'
+logging.basicConfig(level=logging_level,
+                    format='%(levelname)s %(message)s')
+
+
 def get_all_datasets():
     # Returns all the datasets available on the ONS API. Also goes and fetches the latest download link (ie most recent version) and the geographic resolution.
+    # Not sure you'll ever need to use this really, but it's useful for building the dataset list right now.
     import requests
     datasets = []
     api_result = {
@@ -29,17 +41,21 @@ def get_all_datasets():
     return datasets
 
 
-def get_ons_dataset(id, filters):
-    # fetches a dataset from the ONS API, and returns a pandas dataframe, with the filters applied
+def quick_data(id, filters):
+    """"
+    fetches a dataset from the ONS API, and returns a pandas dataframe, with the filters applied
+    """
     import requests
     import pandas as pd
-
+    filters = build_filter(id, filters)
     dataset_url = "https://api.beta.ons.gov.uk/v1/datasets/" + id
     dataset_metadata = requests.get(dataset_url).json()
     latest_version = dataset_metadata['links']['latest_version']['href']
     data_url = latest_version + "/observations?"
+
     for key in filters.keys():
         data_url += key + "=" + str(filters[key]) + "&"
+
     dataset = requests.get(data_url).json()
     df = pd.json_normalize(dataset['observations'])
 
@@ -51,4 +67,104 @@ def get_ons_dataset(id, filters):
             good_columns.append(bits[1])
     df = df[good_columns]
     df = df.rename(columns={"observation": dataset['unit_of_measure']})
+    return df
+
+
+def get_dimensions(id):
+    # returns the dimensions of a dataset, and the options for each dimension
+    import requests
+    dataset_url = "https://api.beta.ons.gov.uk/v1/datasets/" + id
+    dataset_metadata = requests.get(dataset_url).json()
+    latest_version = dataset_metadata['links']['latest_version']['href']
+    d_url = latest_version + "/dimensions"
+    dimensions = requests.get(d_url).json()
+    return dimensions['items']
+
+
+def get_options(id):
+    import requests
+    dimensions = get_dimensions(id)
+    out = []
+    for dimension in dimensions:
+        options_result = requests.get(
+            dimension['links']['options']['href'] + "?limit=1000").json()
+        options = []
+        for option in options_result['items']:
+            options.append(option['option'])
+        out.append({
+            str(dimension['name']): options
+        })
+    return out
+
+
+def build_filter(dataset, filters):
+    import requests
+    dimensions = get_dimensions(dataset)
+    for dimension in dimensions:
+        # print(dimension['links']['options']['href'])
+        if dimension['name'] not in filters.keys():
+            options = requests.get(
+                dimension['links']['options']['href']).json()
+            for options in options['items']:
+                if options['option'] == 'all' or options['option'] == 'total':
+                    filters[dimension['name']] = options['option']
+                    break
+    return filters
+
+
+def build_dimensions(dimensions):
+    # builds the dimensions part of the POST request
+    new_dimensions = []
+    for dimension in dimensions.keys():
+        if type(dimensions[dimension]) is str:
+            new_dimensions.append({
+                "name": dimension,
+                "options": [dimensions[dimension]]
+            })
+        else:
+            new_dimensions.append({
+                "name": dimension,
+                "options": dimensions[dimension]
+            })
+    return new_dimensions
+
+
+def get_dataset(dataset, dimensions):
+    # POST to ONS with a filter, and get the data back
+    import requests
+    import pandas as pd
+    import time
+    import io
+
+    dataset_url = "https://api.beta.ons.gov.uk/v1/datasets/" + dataset
+    dataset_metadata = requests.get(dataset_url).json()
+    latest_version = dataset_metadata['links']['latest_version']['href']
+    logger.info('Found latest version of dataset ' + dataset)
+    latest_version_metadata = requests.get(latest_version).json()
+    post_data = {
+        "dataset": {
+            "id": dataset,
+            "edition": latest_version_metadata['edition'],
+            "version": latest_version_metadata['version']
+        },
+        "dimensions": build_dimensions(dimensions),
+    }
+
+    logger.info('Posting filter...')
+    filter_metadata = requests.post(
+        "https://api.beta.ons.gov.uk/v1/filters?submitted=true", json=post_data).json()
+    output_url = filter_metadata['links']['filter_output']['href']
+
+    logger.info('Waiting for ONS to prepare download...')
+    download_metadata = requests.get(output_url).json()
+    while download_metadata['state'] == 'created':
+        time.sleep(1)
+        download_metadata = requests.get(output_url).json()
+
+    logger.info('Downloading file...')
+    download_url = download_metadata['downloads']['csv']['href']
+    csv = requests.get(download_url)
+    df = pd.read_csv(io.StringIO(csv.text))
+    logger.info('Download complete')
+
     return df
